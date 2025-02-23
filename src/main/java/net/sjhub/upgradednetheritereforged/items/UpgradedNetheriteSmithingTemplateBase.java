1ec85cbdfd386ca928b68051168afd7cb4c1d582
sjhub <sj.hub9796@gmail.com>
2025-02-24 05:09:16 +0900

    update component, fancy color

--- src/main/java/net/sjhub/upgradednetheritereforged/items/UpgradedNetheriteSmithingTemplateBase.java
+++ src/main/java/net/sjhub/upgradednetheritereforged/items/UpgradedNetheriteSmithingTemplateBase.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 public class UpgradedNetheriteSmithingTemplateBase extends SmithingTemplateItem {
 
-    private static final Component TRANSCEND_UPGRADE = Component.translatable("upgrade.minecraft.netherite_upgrade").withStyle(ChatFormatting.GRAY);
-    private static final Component TRANSCEND_APPLIES_TO = Component.translatable("item.minecraft.smithing_template.netherite_upgrade.applies_to").withStyle(ChatFormatting.BLUE);
-    private static final Component TRANSCEND_INGREDIENTS = Component.translatable("item.minecraft.smithing_template.netherite_upgrade.ingredients").withStyle(ChatFormatting.BLUE);
-    private static final Component TRANSCEND_BASE_SLOT_DESCRIPTION = Component.translatable("item.minecraft.smithing_template.netherite_upgrade.base_slot_description");
-    private static final Component TRANSCEND_ADDITIONS_SLOT_DESCRIPTION = Component.translatable("item.minecraft.smithing_template.netherite_upgrade.additions_slot_description");
+    private static final Component TRANSCEND_UPGRADE = Component.translatable("upgrade.upgradednetherite_reforged.netherite_transcend").withStyle(ChatFormatting.GRAY);
+    private static final Component TRANSCEND_APPLIES_TO = Component.translatable("item.upgradednetherite_reforged.netherite_transcend_smithing_template.applies_to").withStyle(ChatFormatting.GOLD);
+    private static final Component TRANSCEND_INGREDIENTS = Component.translatable("item.upgradednetherite_reforged.netherite_transcend_smithing_template.ingredients").withStyle(ChatFormatting.GOLD);
+    private static final Component TRANSCEND_BASE_SLOT_DESCRIPTION = Component.translatable("item.upgradednetherite_reforged.netherite_transcend_smithing_template.base_slot_description");
+    private static final Component TRANSCEND_ADDITIONS_SLOT_DESCRIPTION = Component.translatable("item.upgradednetherite_reforged.netherite_transcend_smithing_template.additions_slot_description");
 
     public UpgradedNetheriteSmithingTemplateBase(Component pAppliesTo, Component pIngredients, Component pUpdradeDescription, Component pBaseSlotDescription, Component pAdditionsSlotDescription, List<ResourceLocation> pBaseSlotEmptyIcons, List<ResourceLocation> pAdditonalSlotEmptyIcons) {
         super(pAppliesTo, pIngredients, pUpdradeDescription, pBaseSlotDescription, pAdditionsSlotDescription, pBaseSlotEmptyIcons, pAdditonalSlotEmptyIcons);
